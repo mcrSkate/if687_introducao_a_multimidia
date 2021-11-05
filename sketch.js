c198bbf7bd8e813e7de757882d793f64dcc8b495
Matheus <mcr@cin.ufpe.br>
2021-11-05 20:49:39 -0300

    versão com bolinha e extremos adicionados

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var Engine = Matter.Engine, 
+const Engine = Matter.Engine, 
     Render = Matter.Render, 
     World = Matter.World, 
     Bodies = Matter.Bodies,
@@ -7,16 +7,49 @@ var Engine = Matter.Engine,
     MouseConstraint = Matter.MouseConstraint;
 
 
-var arena = new Arena();
-var engine = Engine.create();
+//var arena = new Arena();
+
+const drawBody = Helpers.drawBody;
+var engine;
+
+var left,right,up,bottom, disco;
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
-  World.add(engine.world, arena.getBodies());
+  
+  createCanvas(windowWidth, windowHeight);
+  engine = Engine.create();
+  engine.world.gravity.y = 0;
+
+  
+  left = Bodies.rectangle(5,windowHeight/2,10,windowHeight, {restitution: 1, isStatic: true, friction: 0});//left
+  right = Bodies.rectangle(windowWidth-5, windowHeight/2, 10, windowHeight, {restitution: 1, isStatic: true, friction: 0});//right
+  up = Bodies.rectangle(windowWidth/2,5,windowWidth-20,10, {restitution: 1, isStatic: true, friction: 0});//top
+  bottom = Bodies.rectangle(windowWidth/2,windowHeight-5,windowWidth-20,10, {restitution: 1, isStatic: true, friction: 0});//down
+  disco = Bodies.circle(windowWidth/2,windowHeight/2,50, {restitution: 1, friction: 0});
+  World.add(engine.world, [left,right,up,bottom,disco]);
+
+  let mouse = Mouse.create(canvas.elt);
+  let mouseParams = {
+    mouse: mouse,
+    constraint: { stiffness: 0.05, angularStiffness: 0 }
+  }
+  mouseConstraint = MouseConstraint.create(engine, mouseParams);
+  mouseConstraint.mouse.pixelRatio = pixelDensity();
+  World.add(engine.world, mouseConstraint);
+
   Engine.run(engine);
+  
 }
 
 function draw() {
-  background(220);
+  background(255);
+  stroke(0);
+  fill(0);
+  
+  drawBody(left);
+  drawBody(right);
+  drawBody(up);
+  drawBody(bottom);
+  drawBody(disco);
   
 }
