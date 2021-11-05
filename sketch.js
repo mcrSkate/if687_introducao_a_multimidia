38f120c0748313c5553fc5dddde0072e649bec47
Matheus <mcr@cin.ufpe.br>
2021-11-05 19:43:50 -0300

    adicionada biblioteca helpers.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
+var Engine = Matter.Engine, 
+    Render = Matter.Render, 
+    World = Matter.World, 
+    Bodies = Matter.Bodies,
+    Body = Matter.Body,
+    Mouse = Matter.Mouse,
+    MouseConstraint = Matter.MouseConstraint;
+
+
+var arena = new Arena();
+var engine = Engine.create();
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(window.innerWidth, window.innerHeight);
+  World.add(engine.world, arena.getBodies());
+  Engine.run(engine);
 }
 
 function draw() {
   background(220);
+  
 }
