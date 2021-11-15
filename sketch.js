8559b3d7651e55f907498d66f1c5888d297a77b6
Matheus <mcr@cin.ufpe.br>
2021-11-14 23:15:17 -0300

    mudanças na imagem do carro e na sua movimentacao

--- sketch.js
+++ sketch.js
@@ -8,8 +8,7 @@ const Engine = Matter.Engine,
     MouseConstraint = Matter.MouseConstraint;
 
 
-//var arena = new Arena();
-var pontos1 = 0, pontos2 = 0;
+var pontos1 = 0, pontos2 = 0, wheels1, wheels2; acellaration1 = 0, acellaration2 = 0;
 
 const drawBody = Helpers.drawBody;
 var engine;
@@ -21,22 +20,26 @@ var carImage1, carImage2;
 function setup() {
   
   createCanvas(windowWidth, windowHeight);
+  wheels1 = 0;
+  wheels2 = 0;
   engine = Engine.create();
+
   engine.world.gravity.y = 0;
 
-  gol1 = Bodies.rectangle(-75,windowHeight/2,1,windowHeight/3, {isStatic:true, restitution:1, name:'gol1'});
-  gol2 = Bodies.rectangle(windowWidth+75,windowHeight/2,1,windowHeight/3, {isStatic:true, restitution:1, name:'gol2'});
-  left1 = Bodies.rectangle(5,windowHeight/6,10,windowHeight/3, {restitution: 1, isStatic: true, friction: 0});//left
-  right1 = Bodies.rectangle(windowWidth-5, windowHeight/6, 10, windowHeight/3, {restitution: 1, isStatic: true, friction: 0});//right
-  left2 = Bodies.rectangle(5,5*windowHeight/6,10,windowHeight/3, {restitution: 1, isStatic: true, friction: 0});//left
-  right2 = Bodies.rectangle(windowWidth-5, 5*windowHeight/6, 10, windowHeight/3, {restitution: 1, isStatic: true, friction: 0});//right
-  up = Bodies.rectangle(windowWidth/2,5,windowWidth-20,10, {restitution: 1, isStatic: true, friction: 0});//top
-  bottom = Bodies.rectangle(windowWidth/2,windowHeight-5,windowWidth-20,10, {restitution: 1, isStatic: true, friction: 0});//down
-  disco = Bodies.circle(windowWidth/2,windowHeight/2,50, {restitution: 1, friction: 0, density: 0.00001, name: 'disco'});//disco
-  car1 = Bodies.circle(windowWidth-100,windowHeight/2,50, {restitution: 0, friction: 0, isStatic: false, density: 0.002, render: {fillStyle: '#ff0000', sprite: {texture: './assets/car1.png'}}});
-  car2 = Bodies.circle(100,windowHeight/2,50, {restitution: 0, friction: 0, density: 0.002});
+  gol1 = Bodies.rectangle(-20,windowHeight/2,1,windowHeight/3, {isStatic:true, restitution:1, name:'gol1'});
+  gol2 = Bodies.rectangle(windowWidth+20,windowHeight/2,1,windowHeight/3, {isStatic:true, restitution:1, name:'gol2'});
+  left1 = Bodies.rectangle(25,windowHeight/6,50,windowHeight/3, {restitution: 1, isStatic: true, friction: 0});//left
+  right1 = Bodies.rectangle(windowWidth-25, windowHeight/6, 50, windowHeight/3, {restitution: 1, isStatic: true, friction: 0});//right
+  left2 = Bodies.rectangle(25,5*windowHeight/6,50,windowHeight/3, {restitution: 1, isStatic: true, friction: 0});//left
+  right2 = Bodies.rectangle(windowWidth-25, 5*windowHeight/6, 50, windowHeight/3, {restitution: 1, isStatic: true, friction: 0});//right
+  up = Bodies.rectangle(windowWidth/2,25,windowWidth-100,50, {restitution: 1, isStatic: true, friction: 0});//top
+  bottom = Bodies.rectangle(windowWidth/2,windowHeight-25,windowWidth-100,50, {restitution: 1, isStatic: true, friction: 0});//down
+  disco = Bodies.circle(windowWidth/2,windowHeight/2,50, {restitution: 1, frictionAir:0, friction: 0, density: 0.00001, name: 'disco', mass:1});
+  car1 = Bodies.rectangle(windowWidth-100,windowHeight/2,80,50, {restitution: 0.5, friction: 0, frictionAir:0.03, density: 0.002, mass:5});
+  car2 = Bodies.rectangle(100,windowHeight/2,80,50, {restitution: 0.5, friction: 0, frictionAir: 0.03, density: 0.002, mass:5});
   World.add(engine.world, [left1,right1,left2,right2,up,bottom,disco,gol1,gol2,car1,car2]);
 
+  
   let mouse = Mouse.create(canvas.elt);
   let mouseParams = {
     mouse: mouse,
@@ -54,38 +57,32 @@ function setup() {
     const bodyB = pairs.bodyB;
     if (bodyA.name === "gol1" && bodyB.name === "disco") {
       pontos2++;
-      Body.setPosition(disco,{x:windowWidth/2,y:windowHeight/2});
-      Body.setVelocity(disco,{x:0,y:0});
-      Body.setPosition(car1,{x:windowWidth-100,y:windowHeight/2});
-      Body.setVelocity(car1,{x:0,y:0});
-      Body.setPosition(car2,{x:100,y:windowHeight/2});
-      Body.setVelocity(car2,{x:0,y:0});
+      resetPoint();
+
     }else if(bodyA.name==="gol2" && bodyB.name==="disco"){
       pontos1++;
-      Body.setPosition(disco,{x:windowWidth/2,y:windowHeight/2});
-      Body.setVelocity(disco,{x:0,y:0});
-      Body.setPosition(car1,{x:windowWidth-100,y:windowHeight/2});
-      Body.setVelocity(car1,{x:0,y:0});
-      Body.setPosition(car2,{x:100,y:windowHeight/2});
-      Body.setVelocity(car2,{x:0,y:0});
+      resetPoint();
     }
     
   });
 }
 
-
-function verifyPositon(){
-  if(disco.x<5){
-    pontos2++;
-  }
-  if(disco.x>windowWidth-5){
-    pontos1++;
-  }
+function resetPoint(){ //deixa tudo com estado inicial
+  Body.setPosition(disco,{x:windowWidth/2,y:windowHeight/2});
+  Body.setVelocity(disco,{x:0,y:0});
+  Body.setPosition(car1,{x:windowWidth-100,y:windowHeight/2});
+  Body.setVelocity(car1,{x:0,y:0});
+  Body.setPosition(car2,{x:100,y:windowHeight/2});
+  Body.setVelocity(car2,{x:0,y:0});
+  Body.setAngle(car1,0);
+  Body.setAngle(car2,0);
+  wheels1=0;
+  wheels2=0;
 }
 
 function preload() {
-  carImage1 = loadImage('./assets/car1.png');
-  carImage2 = loadImage('./assets/car2.png');
+  carImage1 = loadImage('./assets/grey.png');
+  carImage2 = loadImage('./assets/blue.png');
 }
 
 function draw() {
@@ -93,9 +90,6 @@ function draw() {
   //verifyPositon();
   stroke(0);
   fill(0);
-  textSize(20)
-  text(pontos1, 100, 100);
-  text(pontos2,windowWidth-100, 100);
   drawBody(gol1);
   drawBody(gol2);
   drawBody(left1);
@@ -105,42 +99,124 @@ function draw() {
   drawBody(up);
   drawBody(bottom);
   drawBody(disco);
-
+  fill(255);
+  textSize(20)
+  text(pontos1, 30, 30);
+  text(pontos2,windowWidth-40, 30);
   checkCar1Control();
   checkCar2Control();
-
   imageMode(CENTER);
-  image(carImage1, car1.position.x, car1.position.y, 120, 100);
-  image(carImage2, car2.position.x, car2.position.y, 120, 100);
-  
+  translate(car1.position.x,car1.position.y);//abaixo sao rotacoes e translacoes para poder alinhar a imagem ao movimento do carro, fazendo com que ela sempre rotacione de acordo com o angulo do body
+  rotate(car1.angle);
+  image(carImage1, 0, 0, 85, 55);
+  rotate(-car1.angle);
+  translate(-car1.position.x, -car1.position.y);
+  translate(car2.position.x,car2.position.y);
+  rotate(car2.angle);
+  image(carImage2, 0, 0, 85, 55);
+  rotate(-car2.angle);
+  translate(-car2.position.x, -car2.position.y);
+  line(car1.position.x, car1.position.y, car1.position.x + car1.velocity.x*10, car1.position.y + car1.velocity.y*10);//mostra o vetor velocidade do carro 1
+  line(car2.position.x, car2.position.y, car2.position.x + car2.velocity.x*10, car2.position.y + car2.velocity.y*10);//mostra o vetor velocidade do carro 2
+  stroke(255,0,0);
+  line(car1.position.x,car1.position.y,car1.position.x + 30*cos(car1.angle+wheels1+PI),car1.position.y + 30*sin(car1.angle+wheels1+PI));//mostra a direcao da roda do carro 1
+  line(car2.position.x,car2.position.y,car2.position.x + 30*cos(car2.angle+wheels2),car2.position.y + 30*sin(car2.angle+wheels2));//mostra a direcao da roda do carro 2
+  if(car1.speed>0.01){//esse if e por cause que tem um bug que a speed nunca zera de verdade, por causa da conversao dos numeros fracionarios, ai e pra não ficar uma notacao cientifica do tipo 4.28487578283723e-25 ou algo do tipo
+    text(Math.round(car1.speed*60), windowWidth-50, windowHeight-30, 30);
+  }
+  else{
+    text(0, windowWidth-50, windowHeight-30, 30);//printa isso aqui em vez da notacao cientifica de numeros muito baixos
+  }
+  if(car2.speed>0.01){//mesma coisa do if acima, mas para o carro 2
+    text(Math.round(car2.speed*60), 50, windowHeight-30, 30);
+  }
+  else{
+    text(0, 50, windowHeight-30, 30);//printa o 0 pro carro 2
+  }
 }
+
 
 function checkCar1Control() {
-  if (keyIsDown(LEFT_ARROW)) {
-    Body.applyForce(car1, {x: car1.position.x, y: car1.position.y}, {x: -0.01, y: 0});
+  if(keyIsDown(RIGHT_ARROW)){//movimentacao das rodas do carro
+    if(wheels1<PI/6){
+      wheels1+=PI/180;
+    }
   }
-  if (keyIsDown(RIGHT_ARROW)) {
-    Body.applyForce(car1, {x: car1.position.x, y: car1.position.y}, {x: 0.01, y: 0});
+  if(keyIsDown(LEFT_ARROW)){
+    if(wheels1>-PI/6){
+      wheels1-=PI/180;
+    }
   }
-  if (keyIsDown(UP_ARROW)) {
-    Body.applyForce(car1, {x: car1.position.x, y: car1.position.y}, {x: 0, y: -0.01});
+  if(!keyIsDown(UP_ARROW)&&!keyIsDown(DOWN_ARROW)){//serve para dar mais suavidade quando nao ta tendo nem aceleracao nem frenagem
+    car1.frictionAir = 0.1;
+    acellaration1 = 0;
+  }else{
+    car1.frictionAir = 0.03;
   }
-  if (keyIsDown(DOWN_ARROW)) {
-    Body.applyForce(car1, {x: car1.position.x, y: car1.position.y}, {x: 0, y: 0.01});
+  if(keyIsDown(UP_ARROW)){ //acelera o carro
+    acellaration1 = -0.1;
+    Body.setAngle(car1, car1.angle+=wheels1/360);
   }
+  if(keyIsDown(DOWN_ARROW)){//freia o carro e, eventualmente, da re
+    acellaration1 = 0.05;
+    Body.setAngle(car1, car1.angle-=wheels1/360);
+  }
+  if(car1.speed<4){//limita a velocidade do carro 1, para nao crescer infinitamente
+    Body.setVelocity(car1, {x:car1.velocity.x+acellaration1*cos(car1.angle+wheels1), y:car1.velocity.y+acellaration1*sin(car1.angle+wheels1)});
+  }
+  Body.setPosition(car1, {x:car1.position.x+car1.velocity.x, y:car1.position.y+car1.velocity.y});//atualiza a posicao do carro de acordo com a velocidade
 }
 
-function checkCar2Control() {
-  if (keyIsDown(65)) { //a
-    Body.applyForce(car2, {x: car2.position.x, y: car2.position.y}, {x: -0.01, y: 0});
+function checkCar2Control() {//mesma coisa que encima, so que pro carro 2
+  //65 a 68 d 83 s 87 w
+  if(keyIsDown(68)){
+    if(wheels2<PI/6){
+      wheels2+=PI/180;
+    }
   }
-  if (keyIsDown(68)) { //d
-    Body.applyForce(car2, {x: car2.position.x, y: car2.position.y}, {x: 0.01, y: 0});
+  if(keyIsDown(65)){
+    if(wheels2>-PI/6){
+      wheels2-=PI/180;
+    }
   }
-  if (keyIsDown(87)) { //w
-    Body.applyForce(car2, {x: car2.position.x, y: car2.position.y}, {x: 0, y: -0.01});
+  if(!keyIsDown(87)&&!keyIsDown(83)){
+    car2.frictionAir = 0.1;
+    acellaration2 = 0;
+  }else{
+    car2.frictionAir = 0.03;
   }
-  if (keyIsDown(83)) { //s
-    Body.applyForce(car2, {x: car2.position.x, y: car2.position.y}, {x: 0, y: 0.01});
+  if(keyIsDown(87)){
+    acellaration2 = 0.1;
+    Body.setAngle(car2, car2.angle+=wheels2/360);
   }
+  if(keyIsDown(83)){
+    acellaration2 = -0.05;
+    Body.setAngle(car2, car2.angle-=wheels2/360);
+  }
+  if(car2.speed<4){
+    Body.setVelocity(car2, {x:car2.velocity.x+acellaration2*cos(car2.angle+wheels2), y:car2.velocity.y+acellaration2*sin(car2.angle+wheels2)});
+  }
+  Body.setPosition(car2, {x:car2.position.x+car2.velocity.x, y:car2.position.y+car2.velocity.y});
+
+
+  //rascunhos
+  /*if (keyIsDown(87)) {
+    Body.setVelocity(car2, {x:min(3,car2.velocity.x+(0.1*cos(car2.angle))), y:min(3,car2.velocity.y+(0.1*sin(car2.angle)))});
+    if (keyIsDown(65)) {
+      Body.setAngle(car2, car2.angle-PI/60);
+    }
+    if (keyIsDown(68)) {
+      Body.setAngle(car2, car2.angle+PI/60);
+    }
+  }
+  if (keyIsDown(83)) {
+    Body.setVelocity(car2, {x:max(-0.2,car2.velocity.x-(0.1*cos(car2.angle))), y:max(-0.2,car2.velocity.y-(0.1*sin(car2.angle)))})
+    if (keyIsDown(65)) {
+      Body.setAngle(car2, car2.angle+PI/180);
+    }
+    if (keyIsDown(68)) {
+      Body.setAngle(car2, car2.angle-PI/180);
+    }
+  }
+  Body.setPosition(car2, {x:car2.position.x+car2.velocity.x, y:car2.position.y+car2.velocity.y});*/
 }
